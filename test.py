7e98161056dbbc3c7e3769b0f913dd4c7e6fefd2
Daniel Gaskill <dgaskill02@gmail.com>
2025-02-09 13:00:27 -0800

    basic test

---
+++ test.py
@@ -0,0 +1,20 @@
+# registry_paths = {
+#     "RecentDocs": r"Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs"
+# }
+
+import json
+
+def load_path(filename):
+  with open(filename, 'r') as file:
+    return json.load(file)
+
+def get_registry_path(key):
+  key = key.lower()
+  return registry_paths.get(key, "key not found")
+
+
+if __name__ == "__main__":
+  registry_paths = load_path('paths.json')
+  key = input("Enter the registry key: ")
+  full_path = get_registry_path(key)
+  print(f"Full registry path: {full_path}")
